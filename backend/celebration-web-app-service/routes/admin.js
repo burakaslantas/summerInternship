db93748ee542a4885c4d094bafd4f48933433e23
Burak Aslantas <58150940+burakaslantas@users.noreply.github.com>
2023-08-15 14:11:08 +0300

    npm install prisma --save-dev npx prisma init --datasource-provider postgresql

--- backend/celebration-web-app-service/routes/admin.js
+++ backend/celebration-web-app-service/routes/admin.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('GET respond with a resource BURAK');
 });
 
-router.post('/', function(req, res) {
-  res.send('POST respond with a resource BURAK');
+router.post('/', async function(req, res) {
+  const { adminName, company, email, emailPassword } = req.body;
+  
+  try {
+    const createdAdmin = await prisma.adminModel.create({
+      data: {
+        adminName,
+        company,
+        email,
+        emailPassword,
+      },
+    });
+    res.status(201).json(createdAdmin);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
 router.put('/', function(req, res) {
