ec341f7f89b1cb0422d91a815781cb4fdad4de23
Burak Aslantas <58150940+burakaslantas@users.noreply.github.com>
2023-08-18 15:38:12 +0300

    event schema updated

--- backend/celebration-web-app-service/routes/event.js
+++ backend/celebration-web-app-service/routes/event.js
@@ -3,28 +3,67 @@ const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+// GET all events
 router.get('/', async function(req, res) {
   try {
     const events = await prisma.eventModel.findMany({
-      where: { isDeleted: 0 }, // Only fetch non-deleted events
+      where: { isDeleted: 0 } // Sadece silinmemiş eventleri getir
     });
-    res.status(200).json(events);
+
+    const responsePayload = await Promise.all(events.map(async event => {
+      const company = await prisma.companyModel.findUnique({
+        where: { id: event.companyId }
+      });
+
+      return { 
+        eventName: event.eventName,
+        company: company ? company.companyName : '',
+        type: event.type,
+        to: event.to,
+        bcc: event.bcc,
+        hour: event.hour,
+        minute: event.minute,
+        date: event.date,
+        imageFile: event.imageFile ? event.imageFile.filePath : null,
+        textTemplate: event.textTemplate,
+        id: event.id
+      };
+    }));
+
+    res.status(200).json(responsePayload);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred' });
   }
 });
 
+// GET a specific event by ID
 router.get('/:id', async function(req, res) {
   const eventId = parseInt(req.params.id);
   
   try {
     const event = await prisma.eventModel.findUnique({
       where: { id: eventId },
+      include: {
+        company: true
+      }
     });
 
     if (event && event.isDeleted === 0) {
-      res.status(200).json(event);
+      const responsePayload = {
+        eventName: event.eventName,
+        company: event.company ? event.company.companyName : '',
+        type: event.type,
+        to: event.to,
+        bcc: event.bcc,
+        hour: event.hour,
+        minute: event.minute,
+        date: event.date,
+        imageFile: event.imageFile ? event.imageFile.filePath : null,
+        textTemplate: event.textTemplate,
+        id: event.id
+      };
+      res.status(200).json(responsePayload);
     } else {
       res.status(404).json({ error: 'Event not found' });
     }
@@ -34,69 +73,133 @@ router.get('/:id', async function(req, res) {
   }
 });
 
+// POST a new event
 router.post('/', async function(req, res) {
-  const { eventName, company, type, to, bcc, hour, minute, date, imageFile, textTemplate } = req.body;
+  const { eventName, type, to, bcc, hour, minute, date, imageFile, textTemplate, company } = req.body;
   
   try {
+    const companyObj = await prisma.companyModel.findUnique({
+      where: { companyName: company }
+    });
+
+    if (!companyObj) {
+      return res.status(404).json({ error: 'Company not found' });
+    }
+
     const createdEvent = await prisma.eventModel.create({
       data: {
         eventName,
-        company,
         type,
         to,
         bcc,
         hour,
         minute,
         date,
-        imageFile,
-        textTemplate
+        imageFile: imageFile ? { create: { filePath: imageFile } } : undefined,
+        textTemplate,
+        company: { connect: { id: companyObj.id } }
       },
     });
-    res.status(201).json(createdEvent);
+
+    const responsePayload = {
+      eventName: createdEvent.eventName,
+      company: companyObj.companyName,
+      type: createdEvent.type,
+      to: createdEvent.to,
+      bcc: createdEvent.bcc,
+      hour: createdEvent.hour,
+      minute: createdEvent.minute,
+      date: createdEvent.date,
+      imageFile: createdEvent.imageFile ? createdEvent.imageFile.filePath : null,
+      textTemplate: createdEvent.textTemplate,
+      id: createdEvent.id
+    };
+
+    res.status(201).json(responsePayload);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred' });
   }
 });
 
+// PUT (update) an existing event by ID
 router.put('/:id', async function(req, res) {
   const eventId = parseInt(req.params.id);
-  const { eventName, company, type, to, bcc, hour, minute, date, imageFile, textTemplate } = req.body;
+  const { eventName, type, to, bcc, hour, minute, date, imageFile, textTemplate, companyId } = req.body;
 
   try {
     const updatedEvent = await prisma.eventModel.update({
       where: { id: eventId },
       data: {
         eventName,
-        company,
         type,
         to,
         bcc,
         hour,
         minute,
         date,
-        imageFile,
+        imageFile: imageFile ? { create: { filePath: imageFile } } : undefined,
         textTemplate,
+        company: {
+          connect: { id: companyId }
+        }
       },
+      include: {
+        company: true
+      }
     });
-    res.status(200).json(updatedEvent);
+
+    const responsePayload = {
+      eventName: updatedEvent.eventName,
+      company: updatedEvent.company ? updatedEvent.company.companyName : '',
+      type: updatedEvent.type,
+      to: updatedEvent.to,
+      bcc: updatedEvent.bcc,
+      hour: updatedEvent.hour,
+      minute: updatedEvent.minute,
+      date: updatedEvent.date,
+      imageFile: updatedEvent.imageFile ? updatedEvent.imageFile.filePath : null,
+      textTemplate: updatedEvent.textTemplate,
+      id: updatedEvent.id
+    };
+
+    res.status(200).json(responsePayload);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred' });
   }
 });
 
+// DELETE an event by ID
 router.delete('/:id', async function(req, res) {
   const eventId = parseInt(req.params.id);
 
   try {
-    const updatedEvent = await prisma.eventModel.update({
+    const deletedEvent = await prisma.eventModel.update({
       where: { id: eventId },
       data: {
-        isDeleted: 1,
+        isDeleted: 1
       },
+      include: {
+        company: true
+      }
     });
-    res.status(200).json(updatedEvent);
+
+    const responsePayload = {
+      eventName: deletedEvent.eventName,
+      company: deletedEvent.company ? deletedEvent.company.companyName : '',
+      type: deletedEvent.type,
+      to: deletedEvent.to,
+      bcc: deletedEvent.bcc,
+      hour: deletedEvent.hour,
+      minute: deletedEvent.minute,
+      date: deletedEvent.date,
+      imageFile: deletedEvent.imageFile ? deletedEvent.imageFile.filePath : null,
+      textTemplate: deletedEvent.textTemplate,
+      id: deletedEvent.id
+    };
+
+    res.status(200).json(responsePayload);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred' });
--- backend/celebration-web-app-service/routes/imgUpload.js
+++ backend/celebration-web-app-service/routes/imgUpload.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 // Set up storage for uploaded files
 const storage = multer.diskStorage({
@@ -23,13 +25,44 @@ const upload = multer({
 
 // Set up a route for file uploads
 router.post('/upload', upload.single('file'), async function(req, res) {
-    // Handle the uploaded file
-    res.send("File uploaded successfully!");
+  try {
+    const { originalname, filename, path: filePath } = req.file;
+
+    // Save file information to the database
+    const createdAttachment = await prisma.attachmentModel.create({
+      data: {
+        originalName: originalname,
+        fileName: filename,
+        filePath: filePath,
+        event: { connect: { id: req.body.eventId } } // Connect the attachment to an event
+      }
+    });
+
+    res.status(201).json(createdAttachment);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
-router.get('/upload', async function(req, res) {
-    // Handle the uploaded file
-    res.send("File uploaded successfully!");
+router.get('/upload/:id', async function(req, res) {
+  const attachmentId = parseInt(req.params.id);
+
+  try {
+    // Retrieve attachment from the database
+    const attachment = await prisma.attachmentModel.findUnique({
+      where: { id: attachmentId },
+    });
+
+    if (attachment) {
+      res.status(200).sendFile(attachment.filePath);
+    } else {
+      res.status(404).json({ error: 'Attachment not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
 module.exports = router;
